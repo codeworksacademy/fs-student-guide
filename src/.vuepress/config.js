70b9391e9c6d3717863393f33dcff0b731001894
Jake Overall <joverall22@gmail.com>
2021-08-16 03:37:46 -0600

    deploy

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -8,56 +8,56 @@ module.exports = config({
   base: "/fs-student-guide/",
   title: 'Codeworks Student guide',
   description: "Codeworks Immersive FullStack Program Resources",
-  // head: [
-  //   ['meta', { name: 'theme-color', content: '#3093d9' }],
-  //   ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-  //   ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-  //   ['meta', { name: 'application-name', content: `${GITHUB_USERNAME} guide` }],
-  //   ['meta', { name: 'ROBOTS', content: 'FOLLOW' }],
-  //   ['meta', { name: 'og:image', content: 'https://bcw.blob.core.windows.net/public/img/8600856373152463' }],
-  //   ['meta', { name: 'og:type', content: 'article' }],
-  //   ['meta', { name: 'og:url', content: `https://${GITHUB_USERNAME}.github.io/fs-student-guide` }],
-  //   ['meta', { name: 'og:title', content: 'Learn to Code. Develop Your Future' }],
-  //   ['meta', { name: 'og:locale', content: 'en_US' }]
-  // ],
+  head: [
+    ['meta', { name: 'theme-color', content: '#3093d9' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['meta', { name: 'application-name', content: `${GITHUB_USERNAME} guide` }],
+    ['meta', { name: 'ROBOTS', content: 'FOLLOW' }],
+    ['meta', { name: 'og:image', content: 'https://bcw.blob.core.windows.net/public/img/8600856373152463' }],
+    ['meta', { name: 'og:type', content: 'article' }],
+    ['meta', { name: 'og:url', content: `https://${GITHUB_USERNAME}.github.io/fs-student-guide` }],
+    ['meta', { name: 'og:title', content: 'Learn to Code. Develop Your Future' }],
+    ['meta', { name: 'og:locale', content: 'en_US' }]
+  ],
   themeConfig: {
-    // pwa: false,
+    pwa: false,
     logo: 'https://bcw.blob.core.windows.net/public/img/2900578872732848',
-    // editLinks: true,
-    // repo: `${GITHUB_USERNAME}/fs-student-guide`,
-    // repoLabel: 'Contribute!',
-    // docsDir: 'src',
-    // docsBranch: 'main',
-    // nav: [
-    //   {
-    //     text: 'Resources',
-    //     link: '/resources/',
-    //   },
-    //   {
-    //     text: 'Vocab',
-    //     link: '/vocab/',
-    //   },
-    //   {
-    //     text: 'Codeworks',
-    //     link: 'https://boisecodeworks.com'
-    //   }
-    // ],
-    // sidebar: {
-    //   '/vocab/': [...getSideBar('vocab', 'Vocabulary')],
-    //   '/resources/': [
-    //     ...getSideBar('resources', 'Student Resources'),
-    //     ...getSideBar('resources/wk1', 'Building Blocks of Web Development'),
-    //     ...getSideBar('resources/wk2', 'Intro to Js'),
-    //     ...getSideBar('resources/wk3', 'Advancing with JS'),
-    //     ...getSideBar('resources/wk4', 'Asynchronous Code'),
-    //     ...getSideBar('resources/wk5', 'Servers with Node/Express'),
-    //     ...getSideBar('resources/wk6', 'Frontend Frameworks with Vue3'),
-    //     ...getSideBar('resources/wk7', 'Advancing with Vue3'),
-    //     ...getSideBar('resources/wk8-9', 'Professional Environments'),
-    //     ...getSideBar('resources/wk10', 'Foundations of C#'),
-    //     ...getSideBar('resources/wk11', 'Dotnet WebApi\'s')
-    //   ]
-    // },
+    editLinks: true,
+    repo: `${GITHUB_USERNAME}/fs-student-guide`,
+    repoLabel: 'Contribute!',
+    docsDir: 'src',
+    docsBranch: 'main',
+    nav: [
+      {
+        text: 'Resources',
+        link: '/resources/',
+      },
+      {
+        text: 'Vocab',
+        link: '/vocab/',
+      },
+      {
+        text: 'Codeworks',
+        link: 'https://boisecodeworks.com'
+      }
+    ],
+    sidebar: {
+      '/vocab/': [...getSideBar('vocab', 'Vocabulary')],
+      '/resources/': [
+        ...getSideBar('resources', 'Student Resources'),
+        ...getSideBar('resources/wk1', 'Building Blocks of Web Development'),
+        ...getSideBar('resources/wk2', 'Intro to Js'),
+        ...getSideBar('resources/wk3', 'Advancing with JS'),
+        ...getSideBar('resources/wk4', 'Asynchronous Code'),
+        ...getSideBar('resources/wk5', 'Servers with Node/Express'),
+        ...getSideBar('resources/wk6', 'Frontend Frameworks with Vue3'),
+        ...getSideBar('resources/wk7', 'Advancing with Vue3'),
+        ...getSideBar('resources/wk8-9', 'Professional Environments'),
+        ...getSideBar('resources/wk10', 'Foundations of C#'),
+        ...getSideBar('resources/wk11', 'Dotnet WebApi\'s')
+      ]
+    },
   },
   // @ts-ignore
   markdown: {
